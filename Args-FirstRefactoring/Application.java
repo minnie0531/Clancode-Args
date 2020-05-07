00e7900b6ea84720ebf57e315ecc8562654ce500
minnie0531 <minheekim0531@gmail.com>
2020-05-07 14:44:38 +0900

    Create Application.java

---
+++ Args-FirstRefactoring/Application.java
@@ -0,0 +1,21 @@
+import java.text.ParseException;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        try {
+            Args arg = new Args("l,p#,d*", args);
+            boolean logging = arg.getBoolean('l');
+            int port = arg.getInt('p');
+            String directory = arg.getString('d');
+
+            System.out.printf("Boolean element : %s, Integer element : %s, String element: %s\n", logging, port, directory);
+            //executeApplication(logging, port, directory);
+        } catch (ParseException e) {
+            System.out.printf("Argument error: %s\n", e.getMessage());
+        }
+
+    }
+}
+
